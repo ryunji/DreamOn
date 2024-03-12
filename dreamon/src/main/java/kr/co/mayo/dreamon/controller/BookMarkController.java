d1bfbd14bd20f6f12b32119caba72ea0868c3ec9
Ryun Ji <ryunji1990@gmail.com>
2024-03-12 15:23:52 +0900

    쿠키로 북마크 처리

--- dreamon/src/main/java/kr/co/mayo/dreamon/controller/BookMarkController.java
+++ dreamon/src/main/java/kr/co/mayo/dreamon/controller/BookMarkController.java
@@ -1,12 +1,21 @@
 package kr.co.mayo.dreamon.controller;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.google.gson.Gson;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
@@ -21,29 +30,68 @@ public class BookMarkController {
     @Autowired
     private InterviewService service;
 
+    @GetMapping("my-list")
+    public String myList(@CookieValue(required = false) String interviewCards
+                       , Model model){
+
+        List<InterviewCard> interveiwCardList;
+        if(interviewCards == null)
+            
+            interveiwCardList = new ArrayList<>();
+        else{
+
+            //Decode
+            String interviewCardStr = URLDecoder.decode(interviewCards, Charset.forName("utf-8"));
+            interveiwCardList = new Gson().fromJson(interviewCardStr, List.class);
+        }
+
+        model.addAttribute("interviewCards", interveiwCardList);
+        return "user/myInterviewCardList";
+    }
+
     //1.인터뷰 카드 나의 인터뷰 카드로 담기.
     @PostMapping("add-interview-card")
     public String addCard(Long id
+                        , @CookieValue(required = false) String interviewCards
                         , HttpServletResponse response){
 
         System.out.println("1.interviewCardId : " + id);
-        //id값으로 인터뷰 카드 정보 1개 조회.
-        InterviewCard card  = service.getById(id);
-        String cardStr      = card.toString();
-        String cardEncoded  = "";
-        try {
+        
+        List<InterviewCard> interveiwCardList;                        
+        {
+            if(interviewCards == null)
+                
+            interveiwCardList = new ArrayList<>();
+            else{
+
+                String interviewCardStr = URLDecoder.decode(interviewCards, Charset.forName("utf-8"));
+                interveiwCardList = new Gson().fromJson(interviewCardStr, List.class);
+            }
+
+            InterviewCard interviewCard  = service.getById(id);
+            interveiwCardList.add(interviewCard);
+        }
+        
+        {
+            //id값으로 인터뷰 카드 정보 1개 조회.
+            String cardStr      = new Gson().toJson(interveiwCardList);     //card.toString();
+            String cardEncoded  = "";
+            try {
+                
+                cardEncoded  = URLEncoder.encode(cardStr, "utf-8");
+            } catch (UnsupportedEncodingException e) {
+                
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
             
-            cardEncoded  = URLEncoder.encode(cardStr, "utf-8");
-        } catch (UnsupportedEncodingException e) {
-            
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            Cookie cardCookie = new Cookie("interviewCards", cardEncoded);
+                   cardCookie.setPath("/");
+    
+            cardCookie.setValue(cardEncoded);
+            response.addCookie(cardCookie);
         }
 
-        Cookie cardCookie = new Cookie("card", cardEncoded);
-            cardCookie.setPath("/");
-
-        response.addCookie(cardCookie);
         return "redirect:/interview/card-list";
     }
 }
