c2e37d288dd0b2863cb18132e277606c77e0c5ec
Ryun Ji <ryunji1990@gmail.com>
2024-03-13 16:04:57 +0900

    수업시간 내용 적용

---
+++ dreamon/src/main/java/kr/co/mayo/dreamon/config/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package kr.co.mayo.dreamon.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+    
+    @Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		    
+            http
+                .csrf(csrf->csrf.disable())                                     //POST 요청 시 
+			    .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/member/**").hasAnyRole("MEMBER", "ADMIN")    //2개 이상의 권한 허용 시.
+                .requestMatchers("/admin/**").hasRole("ADMIN")                  //1개의 권한만 허용 시.
+                .anyRequest().permitAll()
+                //.requestMatchers("/", "/home").permitAll()
+				//.anyRequest().authenticated()
+			)
+			.formLogin((form) -> form
+				.loginPage("/user/signIn")
+				.permitAll()
+			)
+			.logout((logout) -> logout
+            .logoutUrl("/user/logout")                                          //로그아웃 시, 스프링이 매칭해주는 url 주소? : header.html에서 로그아웃 버튼 href에 기입한 url.
+            .logoutSuccessUrl("/index")                                         //로그아웃 처리 후 index 페이지로 이동시킴.
+            .permitAll());
+
+		return http.build();
+	}
+
+    //2.사용자 정보를 제공하는 서비스
+	@Bean
+	public UserDetailsService userDetailsService() {
+		
+        //2.1.사용자 정보를 담고 있는 자료형
+        UserDetails user = User.builder()                                       //기본 코드 : withDefaultPasswordEncoder()
+                               .username("grj90")
+                               .password("{noop}11111")
+                               .roles("MEMBER", "ADMIN")
+                               .build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+
+}
--- dreamon/src/main/java/kr/co/mayo/dreamon/controller/admin/HomeController.java
+++ dreamon/src/main/java/kr/co/mayo/dreamon/controller/admin/HomeController.java
@@ -1,18 +1,17 @@
-package kr.co.mayo.dreamon.controller;
+package kr.co.mayo.dreamon.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-@RequestMapping("/")
+@Controller("adminHomeController")
+@RequestMapping("admin")
 public class HomeController {
     
-
     @GetMapping("index")
-    public String index(){
+    public String index(Model model){
 
-        return "index";
-    }
+        return "admin/index";
+    }   
 }
